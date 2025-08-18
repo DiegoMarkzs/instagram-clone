e2973175fd819c31ce928313599077ec45b31fd0
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-08-17 23:56:43 -0300

    Atualizar o UserRepositoryIntegrationTest.java

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryIntegrationTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryIntegrationTest.java
@@ -11,9 +11,20 @@ public class UserRepositoryIntegrationTest {
     //@Test
     void DadoUsuario_quandoSalvar_PersistirNoBanco(){
 
-    }
+    }   
 
+    void DadoUsuario_QuandoBuscarPorId_RetornarUsuario(){
+User usuario = new User();
+        usuario.setNome("Maria");
+        usuario.setEmail("maria@email.com");
+        usuario.setSenha("123456");
+        usuario = userRepository.save(usuario);
 
+        Optional<User> usuarioEncontrado = userRepository.findById(usuario.getId());
+        assertTrue(usuarioEncontrado.isPresent());
+        assertEquals("Maria", usuarioEncontrado.get().getNome());
+        assertEquals("maria@email.com", usuarioEncontrado.get().getEmail());
 
+}
 
 }
