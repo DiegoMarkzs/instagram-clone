6ecdb0002ac7568b17b6216221273df1ea4cb3bd
DiegoMarkzs <119617704+DiegoMarkzs@users.noreply.github.com>
2025-08-18 01:36:21 -0300

    pequenas mudancas

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryIntegrationTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryIntegrationTest.java
@@ -1,3 +1,9 @@
+package br.edu.ifpb.instagram.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import br.edu.ifpb.instagram.repository.UserRepository;
 
 @DataJpaTest
