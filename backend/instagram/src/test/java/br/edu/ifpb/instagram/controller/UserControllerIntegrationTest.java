e759428e973914c4bbcdde9de9821c9977517da4
Yasmin <yasb367@gmail.com>
2025-08-21 11:22:16 -0300

    testes de excluir no controller

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerIntegrationTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/controller/UserControllerIntegrationTest.java
@@ -1,41 +1,54 @@
 package br.edu.ifpb.instagram.controller;
 
+import br.edu.ifpb.instagram.model.dto.UserDto;
+import br.edu.ifpb.instagram.model.response.UserDetailsResponse;
+import br.edu.ifpb.instagram.repository.UserRepository;
+import br.edu.ifpb.instagram.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import br.edu.ifpb.instagram.repository.UserRepository;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
+    @MockitoBean
     private UserRepository userRepository;
 
+    @MockitoBean
+    private UserServiceImpl userService;
 
-    //@Test
-    void dadoUsuario_quandoCriar_RetornarUsuarioCriado() throws Exception{
+    @Test
+    @WithMockUser(username = "johndoe", roles = { "USER" })
+    void testDeleteUser() throws Exception {
+        Long userId = 1L;
 
+        Mockito.doNothing().when(userService).deleteUser(userId);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/users/{id}", userId)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("user was deleted!"));
+
+        Mockito.verify(userService, Mockito.times(1)).deleteUser(userId);
     }
-
-
-
-
-
-
-
-
 }
